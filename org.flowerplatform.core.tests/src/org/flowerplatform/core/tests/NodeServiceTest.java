a2dff25781d8c64572b54f649ee51fec3c37918e
cristian-spiescu <cristian.spiescu@crispico.com>
2014-02-04 13:00:14 +0200

    #29 - added .core.tests project

---
+++ org.flowerplatform.core.tests/src/org/flowerplatform/core/tests/NodeServiceTest.java
@@ -0,0 +1,19 @@
+package org.flowerplatform.core.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NodeServiceTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
