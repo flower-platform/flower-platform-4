ed861f2d403b1b1fb929ea5f64442ded4a201f02
Solomon Sebastian <sebastian.solomon@crispico.com>
2014-02-11 12:46:23 +0200

    #32 - delete folder content

--- org.flowerplatform.core/src/org/flowerplatform/core/file/FileRemoveNodeController.java
+++ org.flowerplatform.core/src/org/flowerplatform/core/file/FileRemoveNodeController.java
@@ -8,15 +8,29 @@ import org.flowerplatform.core.node.remote.Node;
  * @author Sebastian Solomon
  */
 public class FileRemoveNodeController extends RemoveNodeController {
-	
+	private IFileAccessController fileAccessController = CorePlugin.getInstance().getFileAccessController();
 	@Override
 	public void removeNode(Node node, Node child) {
-		IFileAccessController fileAccessController = CorePlugin.getInstance().getFileAccessController();
+		
 		try {
-			fileAccessController.delete(fileAccessController.getFile(child.getId()));
+			deleteFolder(fileAccessController.getFile(child.getId()));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	private void deleteFolder(Object folder) {
+		Object[] files = fileAccessController.listFiles(folder);
+	    if(files != null) { 
+	        for(Object f: files) {
+	            if(fileAccessController.isDirectory(f)) {
+	                deleteFolder(f);
+	            } else {
+	                fileAccessController.delete(f);
+	            }
+	        }
+	    }
+	    fileAccessController.delete(folder);
 	}
 
 }
