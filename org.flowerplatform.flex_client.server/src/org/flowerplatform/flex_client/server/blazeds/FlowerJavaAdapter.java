527de9133fd172bb857a89f4b641556b684622b1
Solomon Sebastian <sebastian.solomon@crispico.com>
2014-02-11 17:27:35 +0200

    #27 - after code review

--- org.flowerplatform.flex_client.server/src/org/flowerplatform/flex_client/server/blazeds/FlowerJavaAdapter.java
+++ org.flowerplatform.flex_client.server/src/org/flowerplatform/flex_client/server/blazeds/FlowerJavaAdapter.java
@@ -2,7 +2,6 @@ package org.flowerplatform.flex_client.server.blazeds;
 
 import java.util.Date;
 
-import org.flowerplatform.util.plugin.AbstractFlowerJavaPlugin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,28 +10,33 @@ import flex.messaging.messages.RemotingMessage;
 import flex.messaging.services.remoting.adapters.JavaAdapter;
 
 /**
+ * log method call execution time
  * @author Sebastian Solomon
  */
 public class FlowerJavaAdapter extends JavaAdapter {
 
-	private final static Logger logger = LoggerFactory.getLogger(AbstractFlowerJavaPlugin.class);
+	private final static Logger logger = LoggerFactory
+			.getLogger(FlowerJavaAdapter.class);
 
 	public Object invoke(Message message) {
-		long startTime, endTime, difference;
-		startTime = new Date().getTime();
-
+		long startTime= new Date().getTime();
 		Object object = super.invoke(message);
-
-		endTime = new Date().getTime();
-		difference = endTime - startTime;
 		
-		String destination = message.getDestination();
-		String operation = "";
-		if (message instanceof RemotingMessage) {
-			operation = ((RemotingMessage) message).getOperation();
+		if (logger.isDebugEnabled()) {
+			long endTime, difference;
+			
+			endTime = new Date().getTime();
+			difference = endTime - startTime;
+
+			String destination = message.getDestination();
+			String operation = "";
+			if (message instanceof RemotingMessage) {
+				operation = ((RemotingMessage) message).getOperation();
+			}
+			logger.debug("[{}ms] {}.{}() invoked", new Object[] { difference,
+					destination, operation });
 		}
-		logger.info("service: " + destination + ", method: " + operation + ", "
-				+ difference + "ms");
 		return object;
 	}
+	
 }
