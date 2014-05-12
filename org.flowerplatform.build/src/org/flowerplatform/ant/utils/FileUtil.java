d66f1f3297b673c5bac1dbf82ab36b22a3a4b53b
mariana.gheorghe <mariana.gheorghe@crispico.com>
2014-05-12 13:21:59 +0300

    #254 - Run ant script to update license headers from the maven build

---
+++ org.flowerplatform.build/src/org/flowerplatform/ant/utils/FileUtil.java
@@ -0,0 +1,82 @@
+/* license-start
+ * 
+ * Copyright (C) 2008 - 2013 Crispico Software, <http://www.crispico.com/>.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details, at <http://www.gnu.org/licenses/>.
+ * 
+ * license-end
+ */
+package org.flowerplatform.ant.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * 
+ * @author Florin
+ * 
+ */
+public class FileUtil {
+
+	public static String readFile(File file) {
+		StringBuilder sb = new StringBuilder();
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			fr = new FileReader(file);
+			br = new BufferedReader(fr);
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (sb.length() > 0) {
+					sb.append(System.getProperty("line.separator"));
+				}
+				sb.append(line);				
+			} 
+			
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+			} catch (IOException e) {
+			}
+		}
+
+		return sb.toString();
+	}
+
+	public static void writeFile(File file, String text) {
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+
+		try {
+			fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
+			bw.write(text);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}
+			} catch (IOException e) {
+			}
+		}
+	}
+}
