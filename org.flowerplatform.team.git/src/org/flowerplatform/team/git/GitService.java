9b5475a4aceb237aff4cc8afacaa7434695a7895
valentinabojan <bojan.valentinacamelia@yahoo.com>
2014-07-18 11:46:54 +0300

    #366 - Popup: Create Structure Diff from Git Commits

---
+++ org.flowerplatform.team.git/src/org/flowerplatform/team/git/GitService.java
@@ -0,0 +1,37 @@
+package org.flowerplatform.team.git;
+
+import java.io.File;
+
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.flowerplatform.core.file.FileControllerUtils;
+
+/**
+ * @author Valentina-Camelia Bojan
+ */
+public class GitService {
+	
+	public boolean validateHash(String hash, String repositoryPath) {				
+		try {
+			//testing if hash is valid
+			Repository repo = GitUtils.getRepository((File) FileControllerUtils.getFileAccessController().getFile(repositoryPath));
+			if(repo == null)
+				return false;
+			
+			ObjectId resolved = repo.resolve(hash);
+			if(resolved == null)
+				return false;
+			
+			//testing if hash exists in the repository
+			RevWalk rw = new RevWalk(repo);
+			rw.parseCommit(resolved);
+		
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;		
+	}
+
+}
