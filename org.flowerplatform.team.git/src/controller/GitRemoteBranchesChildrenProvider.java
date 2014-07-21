3e8f3744967daa863fde506d33518a98678754fd
eduard-cojocea <eduard.cojocea@crispico.com>
2014-07-21 17:54:43 +0300

    Added children provider skeleton

---
+++ org.flowerplatform.team.git/src/controller/GitRemoteBranchesChildrenProvider.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.util.List;
+
+import org.flowerplatform.core.node.NodeService;
+import org.flowerplatform.core.node.controller.IChildrenProvider;
+import org.flowerplatform.core.node.remote.Node;
+import org.flowerplatform.core.node.remote.ServiceContext;
+import org.flowerplatform.util.controller.AbstractController;
+
+public class GitRemoteBranchesChildrenProvider extends AbstractController implements IChildrenProvider {
+
+	@Override
+	public List<Node> getChildren(Node node, ServiceContext<NodeService> context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren(Node node, ServiceContext<NodeService> context) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
