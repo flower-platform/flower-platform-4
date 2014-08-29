763154d2ad82c2618719683e9c27e097516259d5
valentinabojan <bojan.valentinacamelia@yahoo.com>
2014-08-29 15:12:32 +0300

    #GH465 - added login with facebook

--- org.flowerplatform.core/src/org/flowerplatform/core/users/UserPrincipal.java
+++ org.flowerplatform.core/src/org/flowerplatform/core/users/UserPrincipal.java
@@ -5,11 +5,24 @@ import java.security.Principal;
 public class UserPrincipal implements Principal { 
     private String username;
     
+    private String email;
+    
+    public UserPrincipal(){}
+    
     public UserPrincipal(String username) { 
         this.username = username; 
     }
     
+    public UserPrincipal(String username, String email) { 
+        this.username = username; 
+        this.email = email;
+    }
+    
     public String getName() { 
         return username; 
+    } 
+    
+    public String getEmail() { 
+        return email; 
     } 
 } 
--- org.flowerplatform.js_client.server/src/org/flowerplatform/js_client/server/UserValidatorServlet.java
+++ org.flowerplatform.js_client.server/src/org/flowerplatform/js_client/server/UserValidatorServlet.java
@@ -16,31 +16,45 @@ public class UserValidatorServlet extends HttpServlet {
 	
     @Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	String loginMethod = request.getParameter("loginMethod");
+    	UserPrincipal user = null;    	
     	
-    	// TODO VB: Get the user's info (username & password)
-    	String name = request.getParameter("name");
-        String password = request.getParameter("password");       
-        
-        // TODO VB: Validate the user who tries to login in.
-        // TODO VB: If valid, redirect the client to the desired page,
-        // TODO VB: else redirect to error page.
-        UserPrincipal user = validateLogin(name, password);         
-        if (user == null) {
-        	response.sendRedirect("/org.flowerplatform.host.web_app/authenticate/loginError.html");
-        } else {
-            HttpSession session = request.getSession();
-            session.setAttribute("userPrincipal", user);
-            String requestURL = (String)session.getAttribute("requestURL");
-            // TODO VB: If the user has accessed directly the login page
-            // TODO VB: the redirect him to a main page.
-            if (requestURL == null) {
-            	response.sendRedirect("/org.flowerplatform.host.web_app/authenticate/loginSuccess.html");
-            } else {
-	            session.removeAttribute("requestURL");
-	            System.out.println(requestURL);
-	            response.sendRedirect(requestURL);
-            }
-        }
+    	switch(loginMethod){
+    	// TODO VB: Login with Facebook
+    	case "facebook": {
+    		String email = request.getParameter("email");       	
+            user = new UserPrincipal("facebookUser", email);
+    		break;
+    	}
+    	// TODO VB: Usual login (username & password)
+    	default:
+    		// TODO VB: Get the user's info (username & password)
+	    	String name = request.getParameter("name");
+	        String password = request.getParameter("password");       
+	        
+	        // TODO VB: Validate the user who tries to login in.	       
+	        user = validateLogin(name, password);       
+    		break;
+    	}
+    	
+    	// TODO VB: If valid, redirect the client to the desired page,
+    	// TODO VB: else redirect to error page.
+    	if (user == null) {
+			response.sendRedirect("/org.flowerplatform.host.web_app/authenticate/loginError.html");
+		} else {
+		    HttpSession session = request.getSession();
+		    session.setAttribute("userPrincipal", user);
+		    String requestURL = (String)session.getAttribute("requestURL");
+		    // TODO VB: If the user has accessed directly the login page
+		    // TODO VB: the redirect him to a main page.
+		    if (requestURL == null) {
+		    	response.sendRedirect("/org.flowerplatform.host.web_app/authenticate/loginSuccess.html");
+		    } else {
+		        session.removeAttribute("requestURL");
+		        response.sendRedirect(requestURL);
+		    }
+		}
+    	
 	}
  
     private UserPrincipal validateLogin(String name, String password) {
