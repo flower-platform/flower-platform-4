dcf2007406ca8f18b1fcec0fb300821f6ea83e32
diana-balutoiu <dianabalutoiu@gmail.com>
2014-09-11 12:59:11 +0300

    #390 - run update license header

--- org.flowerplatform.tests/src/org/flowerplatform/tests/EclipseDependentSuite.java
+++ org.flowerplatform.tests/src/org/flowerplatform/tests/EclipseDependentSuite.java
@@ -1,3 +1,18 @@
+/* license-start
+ * 
+ * Copyright (C) 2008 - 2014 Crispico Software, <http://www.crispico.com/>.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details, at <http://www.gnu.org/licenses/>.
+ * 
+ * license-end
+ */
 package org.flowerplatform.tests;
 
 import java.io.File;
